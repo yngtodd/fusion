c711d9e0c7d63edf9edb314dfa943dc91faaa459
Young, Todd <youngmt1@ornl.gov>
2019-11-23 13:54:56 -0500

    Change name for pypi
    There was already a project named fusion.

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@
 
 from setuptools import setup, find_packages
 
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
+
 
 setup(
     author="Todd Young",
@@ -25,12 +27,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     description="An example kernel fusion Pytorch extension.",
@@ -39,7 +35,7 @@ setup(
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='fusion',
-    name='fusion',
+    name='kernelfusion',
     packages=find_packages(include=['fusion']),
     setup_requires=setup_requirements,
     test_suite='tests',
